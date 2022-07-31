b4e1d2b2d71b12f250c2aa3c7b76bffbad3181cb
Mikey191 <desertigorb@gmail.com>
2022-07-31 17:01:16 +0300

    end

--- structLeason/Source.cpp
+++ structLeason/Source.cpp
@@ -26,92 +26,184 @@ struct film
 	double price;//цена диск
 };
 
-
-void ShowList(film* &a, int &size)
+void ShowList(film* &a, int &size)//показывает все добавленные фильмы
 {
 	for (size_t i = 0; i < size; i++)
 	{
-		cout << a[i].title << endl;
-		cout << a[i].director << endl;
-		cout << a[i].genre << endl;
-		cout << a[i].rating << endl;
-		cout << a[i].price << endl;
+		cout << "Title : " << a[i].title << endl;
+		cout << "Director by : " << a[i].director << endl;
+		cout << "Genre : " << a[i].genre << endl;
+		cout << "Rating : " << a[i].rating << endl;
+		cout << "Price : " << a[i].price << endl;
 	}
 	cout << "\n";
+	system("pause");
 }
 
-void ShowOneFilm(film a)
+void ShowOneFilm(film a)//показываает один фильм
 {
-	cout << a.title << endl;
-	cout << a.director << endl;
-	cout << a.genre << endl;
-	cout << a.rating << endl;
-	cout << a.price << endl;
+	cout << "Title : " << a.title << endl;
+	cout << "Director by : " << a.director << endl;
+	cout << "Genre : " << a.genre << endl;
+	cout << "Rating : " << a.rating << endl;
+	cout << "Price : " << a.price << endl;
 }
 
-//функция добавления фильма вручную
-film addFilm()
+film addFilm()//функция добавления фильма вручную
 {
 	film temp;
 	cout << "Film title: ";
-	cin >> temp.title;
+	cin.getline(temp.title, 20);
+	cin.getline(temp.title, 20);
 	cout << "Film Director: ";
-	cin >> temp.director;
+	cin.getline(temp.director, 20);
 	cout << "Film genre: ";
-	cin >> temp.genre;
+	cin.getline(temp.genre, 20);
 	cout << "Film rating: ";
 	cin >> temp.rating;
 	cout << "Film price: ";
 	cin >> temp.price;
-	//cout << "U add film" << endl;
-	//ShowOneFilm(temp);
 	return temp;
 }
+//
+//void pushBackList(film*& arr, int &sizea) //почему эта функция работает некорректно???
+//{
+//	film* newarr = new film[sizea + 1];
+//	for (size_t i = 0; i < sizea; i++) 
+//	{
+//		newarr[i] = arr[i];
+//	}
+//	newarr[sizea++] = addFilm();
+//
+//	delete[] arr;
+//	arr = newarr;
+//}
 
-void pushBackList(film*& arr, int &sizea)
+void pushBackList(film* &a, int& sizea)//доавление нового фильма вконец списка
 {
-	film* newarr = new film[sizea + 1];
-	for (size_t i = 0; i < sizea; i++) 
+	film* tmp;
+	tmp = a;
+	a = new film[sizea + 1];
+	for (size_t i = 0; i < sizea; i++)
 	{
-		newarr[i] = arr[i];
+		a[i] = tmp[i];
 	}
-	newarr[sizea] = addFilm();
-	sizea++;
-
-	delete[] arr;
-	arr = newarr;
+	a[sizea++] = addFilm();
+	delete[] tmp;
 }
 
-
-void menu(film* b, int &sizeb)
+void searchTitle(film* &a, int &sizea)
 {
+	//Функция strcmp() 
+	//осуществляет лексикографическую проверку двух строк, 
+	//оканчивающихся нулевыми символами, и возвращает целое число со следующим 
+	//значением:
+	//меньше 0 если str1 < str2
+	// 0 если str1 = str2
+	//больше 0 если str1 > str2
+	//
+	cout << "Enter title: " << endl;
+	film s;
+	cin >> s.title;
+
+	for (size_t i = 0; i < sizea; i++)
+	{
+		if (strcmp(a[i].title, s.title) == 0)
+			ShowOneFilm(a[i]);
+	}
+	system("pause");
+}
+
+void searchGenre(film*& a, int& sizea)
+{
+	cout << "Enter genre: " << endl;
+	film s;
+	cin >> s.genre;
+
+	for (size_t i = 0; i < sizea; i++)
+	{
+		if (strcmp(a[i].genre, s.genre) == 0)
+			ShowOneFilm(a[i]);
+	}
+	system("pause");
+}
+
+void searchDirector(film*& a, int& sizea)
+{
+	cout << "Enter Director: " << endl;
+	film s;
+	cin >> s.director;
+
+	for (size_t i = 0; i < sizea; i++)
+	{
+		if (strcmp(a[i].director, s.director) == 0)
+			ShowOneFilm(a[i]);
+	}
+	system("pause");
+}
+
+void MostPopFilmGenre(film*& a, int& sizea)
+{
+	cout << "Enter Genre: ";
+	film s;
+	cin >> s.genre;
+	int b=0, tmp=a[0].rating;
+	for (size_t i = 0; i < sizea; i++)
+	{
+		if (strcmp(a[i].genre, s.genre) == 0)
+		{
+			if (a[i].rating > tmp)
+			{
+				tmp = a[i].rating;
+				b = i;
+			}
+		}
+	}
+	cout << "Most popular film in genre " << a[b].genre << " is " << a[b].title << endl;
+	cout << " with rating : " << a[b].rating << endl;
+	system("pause");
+}
+
+void menu(film* &b, int &sizeb)
+{
+	cout << "\tHello, world!\n";
+	cout << "Add first film" << endl;
+	cout << "Skip first add " << endl;
 	b[0] = addFilm();
-	cout << "\tHello\n";
-	cout << "Menu: 1 - search by name\n";
-	cout << "Menu: 2 - search by genre\n";
-	cout << "Menu: 3 - search by director\n";
-	cout << "Menu: 4 - most popular movie\n";
-	cout << "Menu: 5 - show all\n";
-	cout << "Menu: 6 - add film\n";
-	cout << "Menu: 0 - exit\n";
 
 	int choice;
 	do
 	{
+		system("cls");
+		cout << "\tMenu\n";
+		cout << "Menu: 1 - search by title\n";
+		cout << "Menu: 2 - search by genre\n";
+		cout << "Menu: 3 - search by director\n";
+		cout << "Menu: 4 - most popular movie\n";
+		cout << "Menu: 5 - show all\n";
+		cout << "Menu: 6 - add film\n";
+		cout << "Menu: 0 - exit\n";
+		cout << "Your choise: ";
 		cin >> choice;
 		switch (choice) 
 		{
-			case 1: break;
-			case 2: break;
-			case 3: break;
-			case 4: break;
+			case 1: 
+				searchTitle(b, sizeb);
+				break;
+			case 2:
+				searchGenre(b, sizeb);
+				break;
+			case 3: 
+				searchDirector(b, sizeb);
+				break;
+			case 4: 
+				MostPopFilmGenre(b, sizeb);
+				break;
 			case 5:
 				ShowList(b, sizeb);
 				break;
 			case 6:
-				cout << "sizelist befor = " << sizeb << endl;
 				pushBackList(b, sizeb);
-				cout << "sizelist after = " << sizeb << endl;
 				break;
 		}
 	} while (choice != 0);
@@ -124,8 +216,7 @@ int main()
 	film* ListFilm = new film[sizelist];
 	
 	menu(ListFilm, sizelist);
-	cout << "SIZE LIST = " << sizelist << endl;
-	
+		
 	delete[] ListFilm;
 
 	return 0;
